18365f776d4bd64845422e1a416c0c72ba47e22c
yuy218 <yuy218@lehigh.edu>
2015-01-26 21:33:45 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////
+//Brian Chen
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[ ] args) {
+      
+//  print the statement with box around it
+    System.out.println("   -----------      ");
+    System.out.println("   | WELCOME |      ");
+    System.out.println("   -----------      ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-Y--U--Y--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("This August my wife and I took an eleven-day, 675-mile bike");
+    System.out.println("tour on our tandem. We circled Lake Ontario.");
+    
+    //test/testtest
+  }    
+}
